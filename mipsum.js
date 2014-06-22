fa518c80809548d98190ba03d9ee67f14b4a6db7
Diego Felipe <diegofelipece@gmail.com>
2014-06-22 14:32:36 -0300

    randomize using array

--- mipsum.js
+++ mipsum.js
@@ -8,19 +8,19 @@ var min = 1,
     max = 20;
 
 // sort function
-function getRandomArbitrary(min, max) { 
+function getRandomNumber(min, max) { 
   n = Math.random() * (max - min) + min;
   return parseInt(n, 10);
 }
 
-var i = 0;
-while (i < max){
+// define the paragraphs
+var paragraphs = ["paragraph1", "paragraph2", "paragraph3", "paragraph4", "paragraph5", "paragraph6", "paragraph7", "paragraph8", "paragraph9", "paragraph10", "paragraph11", "paragraph12", "paragraph13", "paragraph14", "paragraph15", "paragraph16", "paragraph17", "paragraph18", "paragraph19", "paragraph20"];
+
+for (var i = 0; i < max; i++){
   // show result
-  var randomResult = getRandomArbitrary(min, max);
+  var randomResult = getRandomNumber(min, max) - 1; // 1 less to adjust with the array value
   console.log(randomResult);
-  i++;
+  console.log(paragraphs[randomResult]);
 }
-
-
 
 
