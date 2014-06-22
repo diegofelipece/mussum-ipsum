192362cc2fa4bd99d20e810670be93a75b858696
Diego Felipe <diegofelipece@gmail.com>
2014-06-22 16:06:18 -0300

    Paragraph random done

--- mipsum.js
+++ mipsum.js
@@ -3,29 +3,26 @@ Mispum.js
 First tests 0.1
 */
 
-// Define the min and max value to sort
-var min = 0,
-    max = 19;// 1 less to adjust with the array index
-
-// sort function
-function getRandomNumber(min, max) { 
-  n = Math.random() * (max - min) + min;
-  return parseInt(n, 10);
-}
-
 // define the paragraphs
 var paragraphs = ["paragraph1", "paragraph2", "paragraph3", "paragraph4", "paragraph5", "paragraph6", "paragraph7", "paragraph8", "paragraph9", "paragraph10", "paragraph11", "paragraph12", "paragraph13", "paragraph14", "paragraph15", "paragraph16", "paragraph17", "paragraph18", "paragraph19", "paragraph20"];
-console.log(paragraphs);
+  
+// How many times u need to loop, this will be set by user
+var turns = 20;
 
-for (var i = 0; i < max; i++){
-  // show result
-  var randomResult = getRandomNumber(min, max)  ; 
-  console.log(paragraphs[randomResult]);
-}
+for (var i = 0; i < turns; i++){
+  // Define the min and max value to sort
+  var min = 1,
+      max = paragraphs.length;
 
-console.log(paragraphs);
-var exclude = paragraphs.lastIndexOf("paragraph19", 19);
-console.log(exclude);
-paragraphs.splice(exclude, 1); //exlude the used value for the array
-console.log(paragraphs);
+  // sort function
+  function getRandomNumber(min, max) { 
+    n = Math.random() * (max - min) + min;
+    return Math.round(n) - 1; // 1 less of the total arrays to adjust with the array index
+  }
+
+  var randomResult = getRandomNumber(min, max);
+  console.log(paragraphs[randomResult]); //print the selected value;
+  paragraphs.splice(randomResult, 1); //exlude the used value for the array
+  max --; //decrease max getRandomNumber
+};
 
