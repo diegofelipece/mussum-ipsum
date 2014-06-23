d61cec3bda2d324ad5527692d680441cb8f30b90
Diego Felipe <diegofelipece@gmail.com>
2014-06-23 00:18:42 -0300

    Upgrade on mipsum.js

--- mipsum.js
+++ mipsum.js
@@ -21,105 +21,106 @@ Function to sort a number
 Creating paragraphs
 ----------------- >
 */
-  // Define quotes
-  var quotes = [
-    "quote1",
-    "quote2",
-    "quote3",
-    "quote4",
-    "quote5",
-    "quote6",
-    "quote7",
-    "quote8",
-    "quote9",
-    "quote10",
-    "quote11",
-    "quote12",
-    "quote13",
-    "quote14",
-    "quote15",
-    "quote16",
-    "quote17",
-    "quote18",
-    "quote19",
-    "quote20",
-    "quote21",
-    "quote22",
-    "quote23",
-    "quote24",
-    "quote25",
-    "quote26",
-    "quote27",
-    "quote28",
-    "quote29",
-    "quote30",
-    "quote31",
-    "quote32",
-    "quote33",
-    "quote34",
-    "quote35",
-    "quote36",
-    "quote37",
-    "quote38",
-    "quote39",
-    "quote40"
-  ],
-  paragraphs = [],
-  pTagBegin = "<p>",
-  pTagEnd = "</p>",
-  hTagBegin = "<h1>",
-  hTagEnd = "</h1>";
+  function creatingParagraphs() {
+    // Define quotes
+    var quotes = [
+      "quote1",
+      "quote2",
+      "quote3",
+      "quote4",
+      "quote5",
+      "quote6",
+      "quote7",
+      "quote8",
+      "quote9",
+      "quote10",
+      "quote11",
+      "quote12",
+      "quote13",
+      "quote14",
+      "quote15",
+      "quote16",
+      "quote17",
+      "quote18",
+      "quote19",
+      "quote20",
+      "quote21",
+      "quote22",
+      "quote23",
+      "quote24",
+      "quote25",
+      "quote26",
+      "quote27",
+      "quote28",
+      "quote29",
+      "quote30",
+      "quote31",
+      "quote32",
+      "quote33",
+      "quote34",
+      "quote35",
+      "quote36",
+      "quote37",
+      "quote38",
+      "quote39",
+      "quote40"
+    ],
+    pTagBegin = "<p>",
+    pTagEnd = "</p>",
+    hTagBegin = "<h1>",
+    hTagEnd = "</h1>";
 
-  var quotesLength = quotes.length,
-      max = quotesLength; //redefine max value
+    window.paragraphs = [];
 
-  // It makes 10 paragraphs
-  for (var y = 0; y < 10; y++){
-    // 1 paragraph == 4 quotes
-    for (var i = 0, tempParagraph = ""; i < 4; i++){
-      // sort function
-      var randomResult = getRandomNumber(min, max);
+    var quotesLength = quotes.length,
+        max = quotesLength; //redefine max value
 
-      var q = quotes[randomResult];
-      tempParagraph += q + " "; // append the quote on a temp string
-      quotes.splice(randomResult, 1); //exlude the used value for the array
-      max --; //decrease max getRandomNumber
+    // It makes 10 paragraphs
+    for (var y = 0; y < 10; y++){
+      // 1 paragraph == 4 quotes
+      for (var i = 0, tempParagraph = ""; i < 4; i++){
+        // sort function
+        var randomResult = getRandomNumber(min, max);
 
-      if (i==3) {
-        // Push the the tem string to the paragraphs array
-        paragraphs.push(tempParagraph);
-        break;
+        var q = quotes[randomResult];
+        tempParagraph += q + " "; // append the quote on a temp string
+        quotes.splice(randomResult, 1); //exlude the used value for the array
+        max --; //decrease max getRandomNumber
+
+        if (i==3) {
+          // Push the the tem string to the paragraphs array
+          paragraphs.push(tempParagraph);
+          break;
+        };
       };
     };
+
+    console.log(paragraphs);
+    sortParagraphs();
   };
-
-  console.log(paragraphs);
-
-/*
-Triggers
------------------ >
-*/
-  // var turnsBt = document.getElementById('turns');
-  // turnsBt.click(console.log('click!'));
-
-  // function triggers(){
-  //   var turns = document.getElementById('turns').value;
-  //   console.log(turns);
-  //   sortParagraphs();
-  //   var kind = document.getElementById('turns').value;
-  //   console.log(turns);
-  // };
-
-
-
-  var pTag = false,
-      hTag = false;
 
 /*
 Sorting paragraphs
 ----------------- >
 */
   function sortParagraphs(){
+    /*
+    Triggers
+    ----------------- >
+    */
+      // var turnsBt = document.getElementById('turns');
+      // turnsBt.click(console.log('click!'));
+
+      // function triggers(){
+      //   var turns = document.getElementById('turns').value;
+      //   console.log(turns);
+      //   sortParagraphs();
+      //   var kind = document.getElementById('turns').value;
+      //   console.log(turns);
+      // };
+      var pTag = false,
+          hTag = false;
+
     var turns = document.getElementById('turns').value;
     console.log(turns);
     // How many paragraphs u need ?, this will be set by user
