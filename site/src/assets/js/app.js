3facca21a460422fa0e9a0a3345906f541b777f6
Diego Esteves <diegofelipece@gmail.com>
2016-04-13 10:23:39 -0300

    Move project to Foundation 6

--- site/src/assets/js/app.js
+++ site/src/assets/js/app.js
@@ -1,1 +1,32 @@
-$(document).foundation();
+'use strict';
+
+var app = (function(document, $) {
+	var docElem = document.documentElement,
+		_userAgentInit = function() {
+			docElem.setAttribute('data-useragent', navigator.userAgent);
+		},
+		_init = function() {
+			$(document).foundation();
+			
+		    // mIpsum -> set the function of trigger call
+			$('#mIpsumTrigger').on('click', function(event) {
+				event.preventDefault();
+
+				// console.log('há!');
+
+				var turns = $('#turns').val(),
+					pTagSet = false,
+					hTagSet = false;
+				mIpsumTrigger(turns, pTagSet, hTagSet);
+			});
+
+			_userAgentInit();
+		};
+	return {
+		init: _init
+	};
+})(document, jQuery);
+
+(function() {
+	app.init();
+})();
