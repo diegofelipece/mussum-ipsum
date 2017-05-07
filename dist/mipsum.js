91b515fe4f4a2a7fe394bbf2a35dd0b0c6ffbe89
Diego Esteves <diegofelipece@gmail.com>
2017-05-07 10:50:03 +0200

    Add minified version of script

--- dist/mipsum.js
+++ dist/mipsum.js
@@ -3,12 +3,14 @@
 /* ---------------
 mipsum.js v2.0.5
 --------------- */
+var mussumMainQuote = "Mussum Ipsum, cacilds vidis litro abertis. ";
 var mussumQuotes = ["Pra lá , depois divoltis porris, paradis. ", "Paisis, filhis, espiritis santis. ", "Mé faiz elementum girarzis, nisi eros vermeio. ", "Manduma pindureta quium dia nois paga. ", "Sapien in monti palavris qui num significa nadis i pareci latim. ", "Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.", "Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. ", "Interagi no mé, cursus quis, vehicula ac nisi. ", "Casamentiss faiz malandris se pirulitá.", "Cevadis im ampola pa arma uma pindureta. ", "Atirei o pau no gatis, per gatis num morreus. ", "Viva Forevis aptent taciti sociosqu ad litora torquent. ", "Copo furadis é disculpa de bebadis, arcu quam euismod magna. ", "Delegadis gente finis, bibendum egestas augue arcu ut est. ", "In elementis mé pra quem é amistosis quis leo. ", "Não sou faixa preta cumpadi, sou preto inteiris, inteiris. ", "Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. ", "Suco de cevadiss deixa as pessoas mais interessantis. ", "Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. ", "Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! ", "Quem manda na minha terra sou euzis! ", "Si num tem leite então bota uma pinga aí cumpadi! ", "Diuretics paradis num copo é motivis de denguis. ", "Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose. ", "A ordem dos tratores não altera o pão duris. ", "Quem num gosta di mim que vai caçá sua turmis! ", "Quem num gosta di mé, boa gentis num é. ", "Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis! ", "Per aumento de cachacis, eu reclamis. ", "Detraxit consequat et quo num tendi nada. ", "Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. ", "Leite de capivaris, leite de mula manquis sem cabeça. ", "Aenean aliquam molestie leo, vitae iaculis nisl. ", "Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. ", "Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. ", "Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. ", "Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus. ", "Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. ", "Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl. ", "Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum. "];
 
 var mIpsum = function mIpsum() {
   var pNum = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
   var quotes = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : mussumQuotes;
-  var maxOfp = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 9999;
+  var mainQuote = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : mussumMainQuote;
+  var maxOfp = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 9999;
 
   /* Function to sort a number
   ----------------- > */
@@ -77,10 +79,11 @@ var mIpsum = function mIpsum() {
     var paragraphs = createParagraphs(pNum, quotes);
 
     for (var i = 0; i < pNum; i++) {
+      result += '<p>';
       if (i === 0) {
-        result += "Mussum Ipsum, cacilds vidis litro abertis. ";
+        result += mainQuote;
       }
-      result += paragraphs[i];
+      result += paragraphs[i] + '</p>';
     }
 
     return result;
