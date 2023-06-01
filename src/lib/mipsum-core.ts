0424d125736fea3f475e97de9acecfb387d11eb7
Diego Esteves <diegofelipece@gmail.com>
2023-06-01 19:11:10 +0200

    feat: fix "self" issue and support node 19

--- src/lib/mipsum-core.ts
+++ src/lib/mipsum-core.ts
@@ -22,28 +22,24 @@ class MussumIpsum {
 
   /** Create */
   createParagraphs(): string[] {
-    const { _options: options } = this
-
     const paragraphs: string[] = []
-    const limit = Math.floor(options.quotes.length / options.pQuotes)
-    const roundsNeeded = Math.ceil(options.pNum / limit)
+    const limit = Math.floor(this._options.quotes.length / this._options.pQuotes)
+    const roundsNeeded = Math.ceil(this._options.pNum / limit)
     const tempParagraphs = []
     for (let i = 0; i < (roundsNeeded * limit); i++) {
       tempParagraphs.push(this.createOneParagraph())
     }
 
-    tempParagraphs.forEach((paragraph, i) => (options.pNum > i) && paragraphs.push(paragraph))
+    tempParagraphs.forEach((paragraph, i) => (this._options.pNum > i) && paragraphs.push(paragraph))
     return paragraphs
   }
 
   createOneParagraph(): string {
-    const { _options: options } = this
-
     const singleParagraph = []
-    const tempQuotes = [...options.quotes]
+    const tempQuotes = [...this._options.quotes]
     let randomLimit = tempQuotes.length
 
-    for (let i = 0; i < options.pQuotes; i++) {
+    for (let i = 0; i < this._options.pQuotes; i++) {
       const thisPosition = (
         Math.round(Math.random() * (randomLimit - 1) + 1) - 1
       ) // get a random position
@@ -57,18 +53,17 @@ class MussumIpsum {
 
   /** Format response */
   format(recievedParagraphs: string[]) {
-    const { _options: options } = this
     const paragraphs = recievedParagraphs    
 
     // add the initial quote
-    paragraphs[0] = `${options.mainQuote} ${paragraphs[0]}`
+    paragraphs[0] = `${this._options.mainQuote} ${paragraphs[0]}`
     
-    const final = (options.resultType === 'array')
+    const final = (this._options.resultType === 'array')
       ? paragraphs
       : paragraphs.reduce((prev, paragraph) => {
 
-        return(options.resultType === 'html')
-          ? `${prev}${options.tagBefore}${paragraph}${options.tagAfter}`
+        return(this._options.resultType === 'html')
+          ? `${prev}${this._options.tagBefore}${paragraph}${this._options.tagAfter}`
           : `${prev}${paragraph}\n\n`
       }, '')
 
@@ -76,19 +71,17 @@ class MussumIpsum {
   }
 
   errorHandler() {
-    const { _options: options } = this
-
-    if (options.pNum <= 0 || !options.pNum) {
+    if (this._options.pNum <= 0 || !this._options.pNum) {
       return 'Is not possible to create 0 paragraphs.'
     }
-    if (options.pNum > options.genLimit) {
-      return `Your request of ${options.pNum} paragraphs, is larger than the defined limit: ${options.genLimit}. You can change that limit using the key 'genLimit' on your options object.`
+    if (this._options.pNum > this._options.genLimit) {
+      return `Your request of ${this._options.pNum} paragraphs, is larger than the defined limit: ${this._options.genLimit}. You can change that limit using the key 'genLimit' on your options object.`
     }
-    if (options.quotes.length < options.pQuotes) {
-      return `Your array have only ${options.quotes.length} quotes, you need at least ${options.pQuotes} quotes to have results.`
+    if (this._options.quotes.length < this._options.pQuotes) {
+      return `Your array have only ${this._options.quotes.length} quotes, you need at least ${this._options.pQuotes} quotes to have results.`
     }
-    if (!['html', 'text', 'array'].includes(options.resultType)) {
-      return `The value '${options.resultType}' given to 'resultType' is not accepted. Choose between: html or text.`
+    if (!['html', 'text', 'array'].includes(this._options.resultType)) {
+      return `The value '${this._options.resultType}' given to 'resultType' is not accepted. Choose between: html or text.`
     }
 
     return false
